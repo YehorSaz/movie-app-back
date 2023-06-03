89d0ff511968bbc00dd64fe42b8566352ebecbcc
YehorSaz <inercoin@gmail.com>
2023-06-03 13:26:58 +0300

    1

---
+++ index.js
@@ -0,0 +1,40 @@
+const test = require('./selenium/test');
+
+const express = require('express');
+const cors = require('cors');
+
+const PORT = process.env.API_HOST || 'http://localhost:3001';
+
+const app = express();
+
+app.use(express.json());
+
+app.use(cors())
+
+app.listen(PORT, () => {
+    console.log('Work')
+    console.log(PORT)
+})
+
+const movieId = [];
+app.post('/api', async (req, res) => {
+    res.setHeader("access-control-allow-credentials", "false");
+    res.setHeader( "Access-Control-Allow-Origin", "*");
+
+    movieId[0] = req.body.movieId;
+    movieId[1] = await test(movieId[0]);
+
+    res.json({
+        status: "ok",
+        movieId
+    });
+})
+app.get('/api', (req, res) => {
+
+    res.json({
+        movieId
+    });
+
+})
+
+module.exports = app;
